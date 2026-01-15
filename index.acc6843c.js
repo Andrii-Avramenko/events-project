98047a10bd0fb347946375087937b9db3505a005
Andrii-Avramenko <162386783+Andrii-Avramenko@users.noreply.github.com>
2026-01-15 00:53:32 +0000

    Deploying to gh-pages from @ Andrii-Avramenko/events-project@89ed3895e49f14ed2cce03e166f853814c6a393c 🚀

---
+++ index.acc6843c.js
@@ -0,0 +1,2 @@
+!async function(){const t=await async function(){try{const t=await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=9RqLQGT6sSDdxR64riYBmSDsAhCzybzg");return await t.json()}catch(t){console.error(t)}}();console.log(t)}();
+//# sourceMappingURL=index.acc6843c.js.map
