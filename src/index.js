106592a05998ebe9a52f88aa4062372b6afae7e3
Andrii Avramenko <andreitvmm228@gmail.com>
2026-01-22 23:30:06 +0000

    Трохи стилізації, початок роботи на пошуком

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
+import countries from './countries.json'
+
+const countriesInput = document.querySelector('.input-countries')
+const countriesDropdown = document.querySelector('.dropdown-countries')
+const countriesBlock = document.querySelector('.countries-block')
+
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 const API_KEY = '9RqLQGT6sSDdxR64riYBmSDsAhCzybzg';
 
 const eventsContainer = document.querySelector('.event-container');
 
-async function getEvents() {
+function toggleCountriesBlock() {
+  countriesBlock.classList.toggle('is-open')
+  if (!countriesBlock.classList.contains('is-open')) return;
+  countries.forEach(country => {
+    const countryItem = document.createElement('div');
+    countryItem.textContent = country.name;
+    countryItem.classList.add('country-item')
+
+    countryItem.addEventListener('click', () => {
+      countriesInput.value = country.name
+      
+    })
+  })
+}
+
+countriesDropdown.addEventListener('click', toggleCountriesBlock)
+
+async function getEvents() { 
+  let params = new URLSearchParams()
+  console.log(params)
   try {
     const response = await fetch(`${BASE_URL}apikey=${API_KEY}&size=20`);
     if (!response.ok) {
@@ -19,8 +44,8 @@ async function getEvents() {
 function renderEvents(events) {
   const markup = events
     .map(event => {
-      return `<li class="event-item" data-id="${event.id}"><img src="${event.images[3].url}" />
-                <p>${event.name}</p><p>${event.dates.start.localDate}</p><p>${event._embedded.venues[0].name}</p></li>`;
+      return `<li class="event-item" data-id="${event.id}"><img src="${event.images[3].url}" class="event-image"/>
+                <p class="event-title">${event.name}</p><p class="event-date">${event.dates.start.localDate}</p><p class="event-location">${event._embedded.venues[0].name}</p></li>`;
     })
     .join('');
   eventsContainer.innerHTML = markup;
