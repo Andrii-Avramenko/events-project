c0a612f2b980764bcc491fb4349faa1143f68b2b
Andrii Avramenko <andreitvmm228@gmail.com>
2026-01-25 00:46:34 +0000

    Пошук країн

--- src/index.js
+++ src/index.js
@@ -3,44 +3,62 @@ import countries from './countries.json';
 const countriesInput = document.querySelector('.input-countries');
 const countriesDropdown = document.querySelector('.dropdown-countries');
 const countriesBlock = document.querySelector('.countries-block');
+const countriesSet = document.querySelector('.countries-set');
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 const API_KEY = '9RqLQGT6sSDdxR64riYBmSDsAhCzybzg';
 
 const eventsContainer = document.querySelector('.event-container');
 
-function toggleCountriesBlock() {
-  countriesBlock.classList.toggle('is-open');
-  if (!countriesBlock.classList.contains('is-open')) return;
+function toggleCountriesBlock(toggle) {
+  if (toggle) {
+    countriesSet.classList.toggle('is-open');
+  } else {
+    countriesSet.classList.add('is-open');
+  }
+  if (!countriesSet.classList.contains('is-open')) return;
+  countriesBlock.innerHTML = '';
+  userInput = countriesInput.value;
   countries.forEach(country => {
+    if (!country.name.toLowerCase().includes(userInput.toLowerCase())) {
+      return;
+    }
     const countryItem = document.createElement('div');
     countryItem.textContent = country.name;
     countryItem.classList.add('country-item');
 
     countryItem.addEventListener('click', () => {
       countriesInput.value = country.name;
+
+      const url = new URL(window.location.href);
+      url.searchParams.set('countryCode', country.countryCode);
+      window.location.replace(url.toString());
+      countriesSet.classList.remove('is-open');
     });
+    countriesBlock.appendChild(countryItem);
   });
 }
 
-countriesDropdown.addEventListener('click', toggleCountriesBlock);
+countriesInput.addEventListener('input', () => toggleCountriesBlock(false));
+countriesDropdown.addEventListener('click', () => toggleCountriesBlock(true));
 
 async function getEvents() {
   const params = new URLSearchParams(window.location.search);
-  const allowedParams = {
-    keyword: params.get('keyword'),
-    page: params.get('page'),
-  }
-  let requestParams = '';
+  // const allowedParams = {
+  //   keyword: params.get('keyword'),
+  //   page: params.get('page'),
+  // }
+  // let requestParams = '';
 
-  Object.entries(allowedParams).forEach(([key, value]) => {
-    if (!!key && !!value) {
-      requestParams = requestParams + `&${key}=${value}`
-    }
-  });
+  // Object.entries(allowedParams).forEach(([key, value]) => {
+  //   if (!!key && !!value) {
+  //     requestParams = requestParams + `&${key}=${value}`
+  //   }
+  // });
   try {
-    console.log(`${BASE_URL}?apikey=${API_KEY}&size=20&${params}`)
-    const response = await fetch(`${BASE_URL}?apikey=${API_KEY}&size=20&${params}`);
+    const response = await fetch(
+      `${BASE_URL}?apikey=${API_KEY}&size=20&${params}`
+    );
     if (!response.ok) {
       throw new Error(`HTTP error ${response.status}`);
     }
